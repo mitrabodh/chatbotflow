8270fbdd8e42b9acbd0a84c9d3b8ae0d4203f86b
mitrabodh <168332807+mitrabodh@users.noreply.github.com>
2024-06-24 14:59:46 +0530

    Updated SendMessageNode.tsx

--- src/components/nodes/SendMessageNode.tsx
+++ src/components/nodes/SendMessageNode.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { Handle, Position } from "reactflow";
 import { AiOutlineMessage } from "react-icons/ai";
 import { RiWhatsappFill } from "react-icons/ri";
-import styles from "./Send-msg-node.module.css"
-import "reactflow/dist/style.css"
-import "./Custom.css"
+import styles from "./Send-msg-node.module.css";
+import "reactflow/dist/style.css";
+import "./Custom.css";
 
 
 export default React.memo(function SendMessageNode({ id, data }: { id: string, data: { label: string } }) {
@@ -35,5 +35,5 @@ export default React.memo(function SendMessageNode({ id, data }: { id: string, d
             <Handle type="source" style={{ background: " rgba(50, 65, 229, 0.84)" }} position={Position.Right} />
             {/* Node target handle end */}
         </div>
-    )
+    );
 })
